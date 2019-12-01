f6d34486db6bd3025fcd7dee5ad92e62e2b850e9
Ivy Zhang <ivy.zhang@wustl.edu>
2019-12-01 09:33:17 -0500

    add gromacs run files, clean up folders, update renumbered

---
+++ scripts/gromacs/run_gromacs_wt.sh
@@ -0,0 +1,33 @@
+
+## PREP STRUCTURE/TOPOLOGY
+# Create topology
+gmx pdb2gmx -f 5udc_final_v2_refmac1_clean_tail_variable_monomer.pdb -o conf.pdb -ff amber99sb-ildn -water tip3p
+
+## SET UP SIMULATION
+# Define simulation box
+gmx editconf -f conf.pdb -o box.pdb -bt dodecahedron -d 0.9
+
+# Solvate simulation box
+gmx solvate -cp box.pdb -cs spc216 -o water.pdb -p topol.top
+
+# Assemble atomic level description of system (.tpr file) 
+gmx grompp -f ions.mdp -c water.pdb -p topol.top -o ions.tpr
+
+# Add chloride atoms
+gmx genion -s ions.tpr -conc 0.150 -neutral -o ions.pdb -nname CL -pname NA -p topol.top
+
+# Assemble .tpr file again (this time for input to mdrun)
+gmx grompp -f em.mdp -c ions.pdb -p topol.top
+
+## RUN SIMULATION
+# Run energy minimization
+gmx mdrun -v -c em.pdb
+
+# Equilibrate the system 
+echo 0 | gmx trjconv -s topol.tpr -f em.pdb -o em.pdb -ur compact -pbc mol
+gmx grompp -f eq.mdp -c em.pdb -p newtop.top
+gmx mdrun -v -ntomp 2 -nice 0 -c eq.gro
+
+# Run MD
+gmx grompp -f md.mdp -c eq.gro -p newtop.top
+gmx mdrun -v -ntomp 2 -nice 0 -c md.gro
