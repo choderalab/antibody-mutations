3c08022245ec151124a9db78a4432939c37e306d
Ivy Zhang <ivy.zhang@wustl.edu>
2019-12-01 13:36:23 -0500

    add read me, update gromacs bash script

--- scripts/gromacs/run_gromacs_wt.sh
+++ scripts/gromacs/run_gromacs_wt.sh
@@ -11,13 +11,13 @@ gmx editconf -f conf.pdb -o box.pdb -bt dodecahedron -d 0.9
 gmx solvate -cp box.pdb -cs spc216 -o water.pdb -p topol.top
 
 # Assemble atomic level description of system (.tpr file) 
-gmx grompp -f ions.mdp -c water.pdb -p topol.top -o ions.tpr
+gmx grompp -f 01_ions.mdp -c water.pdb -p topol.top -o ions.tpr
 
 # Add chloride atoms
 gmx genion -s ions.tpr -conc 0.150 -neutral -o ions.pdb -nname CL -pname NA -p topol.top
 
 # Assemble .tpr file again (this time for input to mdrun)
-gmx grompp -f em.mdp -c ions.pdb -p topol.top
+gmx grompp -f 02_em.mdp -c ions.pdb -p topol.top
 
 ## RUN SIMULATION
 # Run energy minimization
@@ -25,9 +25,9 @@ gmx mdrun -v -c em.pdb
 
 # Equilibrate the system 
 echo 0 | gmx trjconv -s topol.tpr -f em.pdb -o em.pdb -ur compact -pbc mol
-gmx grompp -f eq.mdp -c em.pdb -p newtop.top
+gmx grompp -f 03_eq.mdp -c em.pdb -p topol.top
 gmx mdrun -v -ntomp 2 -nice 0 -c eq.gro
 
 # Run MD
-gmx grompp -f md.mdp -c eq.gro -p newtop.top
+gmx grompp -f 04_md.mdp -c eq.gro -p topol.top
 gmx mdrun -v -ntomp 2 -nice 0 -c md.gro
